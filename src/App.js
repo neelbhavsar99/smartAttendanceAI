4972716848f05f8207f8aa3eefacb1aa5753780d
Neel Bhavsar <neel.bhavsar@ryerson.ca>
2020-03-25 23:39:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Footer';
+import Heading from "./components/Heading";
+
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <Heading />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           My awesome react App. How you doing today?
           NB
         </p>
+        
         <Footer/>
         
     
--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 
 function Footer(){
+    const year = new Date().getFullYear();
     return (
     <div>
-        <h1 className = "footer" >Copyright Neel Bhavsar 2020</h1>
+        <h1 className = "footer" >Copyright Neel Bhavsar ⓒ {year}</h1>
     </div>);
   
 }
