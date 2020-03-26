ec21cf360021a794f82901fd8785a9f1a819fa76
Neel Bhavsar <neel.bhavsar@ryerson.ca>
2020-03-25 20:56:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Footer';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           My awesome react App. How you doing today?
+          NB
         </p>
+        <Footer/>
+        
     
       </header>
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Footer(){
+    return (
+    <div>
+        <h1 className = "footer" >Copyright Neel Bhavsar 2020</h1>
+    </div>);
+  
+}
+
+export default Footer;
